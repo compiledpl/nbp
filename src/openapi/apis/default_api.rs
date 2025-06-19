52d69c039c0d901f967324a47ac2f225ffbdd43e
Błażej Witczak <blazej.witczak@nsure.com>
2025-06-19 21:49:12 +0200

    Get gold exchange rates in specified date range

--- src/openapi/apis/default_api.rs
+++ src/openapi/apis/default_api.rs
@@ -22,6 +22,13 @@ pub enum CenyzlotaGetError {
     UnknownValue(serde_json::Value),
 }
 
+/// struct for typed errors of method [`cenyzlota_start_date_end_date_get`]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum CenyzlotaStartDateEndDateGetError {
+    UnknownValue(serde_json::Value),
+}
+
 /// struct for typed errors of method [`cenyzlota_today_get`]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
@@ -75,6 +82,43 @@ pub async fn cenyzlota_get(configuration: &configuration::Configuration, ) -> Re
     } else {
         let content = resp.text().await?;
         let entity: Option<CenyzlotaGetError> = serde_json::from_str(&content).ok();
+        Err(Error::ResponseError(ResponseContent { status, content, entity }))
+    }
+}
+
+pub async fn cenyzlota_start_date_end_date_get(configuration: &configuration::Configuration, start_date: &str, end_date: &str) -> Result<Vec<models::GoldPrice>, Error<CenyzlotaStartDateEndDateGetError>> {
+    // add a prefix to parameters to efficiently prevent name collisions
+    let p_start_date = start_date;
+    let p_end_date = end_date;
+
+    let uri_str = format!("{}/cenyzlota/{startDate}/{endDate}/", configuration.base_path, startDate=crate::openapi::apis::urlencode(p_start_date), endDate=crate::openapi::apis::urlencode(p_end_date));
+    let mut req_builder = configuration.client.request(reqwest::Method::GET, &uri_str);
+
+    if let Some(ref user_agent) = configuration.user_agent {
+        req_builder = req_builder.header(reqwest::header::USER_AGENT, user_agent.clone());
+    }
+
+    let req = req_builder.build()?;
+    let resp = configuration.client.execute(req).await?;
+
+    let status = resp.status();
+    let content_type = resp
+        .headers()
+        .get("content-type")
+        .and_then(|v| v.to_str().ok())
+        .unwrap_or("application/octet-stream");
+    let content_type = super::ContentType::from(content_type);
+
+    if !status.is_client_error() && !status.is_server_error() {
+        let content = resp.text().await?;
+        match content_type {
+            ContentType::Json => serde_json::from_str(&content).map_err(Error::from),
+            ContentType::Text => return Err(Error::from(serde_json::Error::custom("Received `text/plain` content type response that cannot be converted to `Vec&lt;models::GoldPrice&gt;`"))),
+            ContentType::Unsupported(unknown_type) => return Err(Error::from(serde_json::Error::custom(format!("Received `{unknown_type}` content type response that cannot be converted to `Vec&lt;models::GoldPrice&gt;`")))),
+        }
+    } else {
+        let content = resp.text().await?;
+        let entity: Option<CenyzlotaStartDateEndDateGetError> = serde_json::from_str(&content).ok();
         Err(Error::ResponseError(ResponseContent { status, content, entity }))
     }
 }
--- tests/goldprice.rs
+++ tests/goldprice.rs
@@ -5,3 +5,13 @@ async fn test_basic() {
     let config = Configuration::new();
     dbg!(api::cenyzlota_get(&config).await.unwrap());
 }
+
+#[tokio::test]
+async fn test_goldprice_date_range() {
+    let config = Configuration::new();
+    dbg!(
+        api::cenyzlota_start_date_end_date_get(&config, "2025-06-10", "2025-06-19")
+            .await
+            .unwrap()
+    );
+}
