2fbc596bf3131d5831d7f484dfe7f21bd1bf4da4
Błażej Witczak <blazej.witczak@nsure.com>
2025-06-19 21:26:20 +0200

    Get currency exchange rates at specified date

--- tests/exchangerates_rates.rs
+++ tests/exchangerates_rates.rs
@@ -10,16 +10,33 @@ async fn test_exchangerates_tables_table_get() {
             .await
             .unwrap()
     );
-
 }
 
 #[tokio::test]
 async fn test_exchangereates_tables_table_last_top_count_get() {
     for top_count in [1, 3, 10, 67] {
-        dbg!(api::exchangerates_tables_table_last_top_count_get(
+        dbg!(
+            api::exchangerates_tables_table_last_top_count_get(
+                &Configuration::new(),
+                TableType::A,
+                top_count
+            )
+            .await
+            .expect("Querying last {topCount} exchange rates tables failed.")
+        );
+    }
+}
+
+#[tokio::test]
+async fn test_exchangerates_rates_table_code_date_get() {
+    dbg!(
+        api::exchangerates_rates_table_code_date_get(
             &Configuration::new(),
             TableType::A,
-            top_count
-        ).await.expect("Querying last {topCount} exchange rates tables failed."));
-    }
+            "USD",
+            "2025-06-10"
+        )
+        .await
+        .unwrap()
+    );
 }
