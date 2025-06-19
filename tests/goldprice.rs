3e3599639fe6a7f22899ae5ac84e2cde19ee8ff0
Błażej Witczak <blazej.witczak@nsure.com>
2025-06-19 21:42:11 +0200

    Get gold exchange rates at specified date

--- tests/goldprice.rs
+++ tests/goldprice.rs
@@ -5,3 +5,13 @@ async fn test_basic() {
     let config = Configuration::new();
     dbg!(api::cenyzlota_get(&config).await.unwrap());
 }
+
+#[tokio::test]
+async fn test_goldprice_date() {
+    let config = Configuration::new();
+    dbg!(
+        api::cenyzlota_date_get(&config, "2025-06-10")
+            .await
+            .unwrap()
+    );
+}
