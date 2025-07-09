5543189db30142e5f6c1e94b8ea048ce31df9142
bwitczak <blaise.witczak@gmail.com>
2025-07-09 07:55:23 +0200

    Merge conflicted PRs into one

--- tests/exchangerates_rates.rs
+++ tests/exchangerates_rates.rs
@@ -23,3 +23,64 @@ async fn test_exchangereates_tables_table_last_top_count_get() {
         ).await.expect("Querying last {topCount} exchange rates tables failed."));
     }
 }
+
+#[tokio::test]
+async fn test_exchangerates_rates_table_code_get() {
+    dbg!(
+        api::exchangerates_rates_table_code_get(&Configuration::new(), TableType::C, "USD")
+            .await
+            .expect("Querying exchange rates for USD failed.")
+    );
+}
+
+#[tokio::test]
+async fn test_exchangerates_tables_table_start_date_end_date_get() {
+    dbg!(
+        api::exchangerates_tables_table_start_date_end_date_get(
+            &Configuration::new(),
+            TableType::A,
+            "2025-06-10",
+            "2025-06-19"
+        )
+        .await
+        .expect("Querying exchange rates for a given date range failed.")
+    );
+}
+
+#[tokio::test]
+async fn test_exchangerates_rates_table_code_date_get() {
+    dbg!(
+        api::exchangerates_rates_table_code_date_get(
+            &Configuration::new(),
+            TableType::A,
+            "USD",
+            "2025-06-10"
+        )
+        .await
+        .unwrap()
+    );
+}
+
+#[tokio::test]
+async fn test_exchangerates_rates_table_code_start_date_end_date_get() {
+    dbg!(
+        api::exchangerates_rates_table_code_start_date_end_date_get(
+            &Configuration::new(),
+            TableType::A,
+            "USD",
+            "2025-06-10",
+            "2025-06-19"
+        )
+        .await
+        .unwrap()
+    );
+}
+
+#[tokio::test]
+async fn test_exchangerates_tables_table_date_get() {
+    dbg!(
+        api::exchangerates_tables_table_date_get(&Configuration::new(), TableType::A, "2025-06-10")
+            .await
+            .unwrap()
+    );
+}
